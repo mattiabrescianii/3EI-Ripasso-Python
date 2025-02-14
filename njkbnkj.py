af060898579269d72e8f75ae38d8d040212127c6
ErosMorozan <20838@studenti.marconiverona.edu.it>
2025-02-14 09:23:05 +0100

    Sistema di votazione per una classe

---
+++ njkbnkj.py
@@ -0,0 +1,3 @@
+Sistema di Votazione per una Classe
+Immagina di essere stato incaricato di creare un programma che gestisca le votazioni di una classe in vari esami. Ogni studente è identificato da un nome,
+e per ciascuno di loro sono registrati i voti per diverse materie. I dati sono organizzati in un dizionario dove ogni chiave è il nome dello studente e il valore è un altro dizionario con le materie e i voti.
