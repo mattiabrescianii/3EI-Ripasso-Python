0a7eb40db209d546cefec05f8e361f193d29f05f
mattiabrescianii <20674@studenti.marconiverona.edu.it>
2025-02-14 10:45:51 +0100

    Update and rename Esercizio Bresciani.py to Esercizio Bresciani.md
    cambiata la traccia dell'esercizio

---
+++ Esercizio Bresciani.md
@@ -0,0 +1,6 @@
+# Esercizio Bresciani
+**Crea un esercizio in Python che...**
+Traduca i numeri romani in quelli in base 10 e viceversa,
+
+l'esercizio usa i dizionari e la concatenazione di stringhe
+
--- Esercizio Bresciani.py
+++
@@ -1,4 +0,0 @@
-#Crea un gioco dell'impiccato giocabile in 2 giocatori,
-#Il primo giocatore deve solamente inserire la parola segreta e si passa alla parte giocabile dal secondo giocatore
-
-#Il secondo giocatore deve indovinare la parola sapendone la lunghezza e alcune parole
