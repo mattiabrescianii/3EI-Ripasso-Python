02241fbf3f7da6cee1d39a3caad3356d7e80b2c8
saladingo1 <20899@studenti.marconiverona.edu.it>
2025-02-14 09:25:27 +0100

    Update Esercizi di Saladino.py

--- Esercizi di Saladino.py
+++ Esercizi di Saladino.py
@@ -1,7 +1,1 @@
-#Il gioco dell'impiccato
-#Scrivi un programma che simula il gioco dell'impiccato con le seguenti funzionalità:
-# -Il programma deve selezionare una parola casuale da un elenco di parole presenti in un file
-# -La parola scelta deve essere mostrata all'utente come una serie di trattini (ad esempio: "_ _ _ _ _ _").
-# -Il giocatore ha un numero limitato di 6 tentativi (che corrispondono agli errori) per indovinare la parola. Il giocatore perde un tentativo quando fa un errore, mentre quando fa giusto si prosegue
-# -Se il giocatore indovina tutte le lettere della parola il programma deve stampare un messaggio di vittoria.
-# -Se il numero di tentativi arriva a zero, il programma deve stampare un messaggio di sconfitta e rivelare la parola.
+
