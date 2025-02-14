2c13c1a246aad0ce2aa1eb1db887df739a68dce8
MohamedLaouina20797 <20797@studenti.marconiverona.edu.it>
2025-02-14 09:33:34 +0100

    Create esercizio fatto da me.py

---
+++ esercizio fatto da me.py
@@ -0,0 +1,4 @@
+#Scrivi un programma che gestisce la vendita dei biglietti per uno stadio. Ogni partita ha una lista di posti disponibili e l'utente può acquistare un biglietto. Il programma deve poter:
+#scegliere una partita con il numero di posti disponibili e il prezzo del biglietto.
+#comprare un biglietto per una partita.
+#Visualizzare le partite con i posti disponibili.
